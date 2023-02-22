37e2617c18506195274da765507f593122d878f1
SakshiShreya <shreyasakshi96@gmail.com>
2023-02-22 23:08:40 +0530

    #22: Add new script in package.json to access localhost apis

--- utils/request.ts
+++ utils/request.ts
@@ -1,5 +1,11 @@
+let baseUrl = "https://gantt-be.onrender.com";
+if (process.env.NEXT_PUBLIC_ENV === "local") {
+  baseUrl = "http://localhost:8000";
+}
+
 export async function get<T>(input: string, query?: Record<string, any>) {
-  const response = await fetch(`https://gantt-be.onrender.com${input}?${new URLSearchParams(JSON.parse(JSON.stringify(query)))}`, {
+  console.log("ENV", process.env.NODE_ENV, process.env.NEXT_PUBLIC_ENV);
+  const response = await fetch(`${baseUrl}${input}?${new URLSearchParams(JSON.parse(JSON.stringify(query)))}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json"
